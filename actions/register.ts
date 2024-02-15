48384135bd266ce829516287a2b917d96dfc733e
apex-devteam <devteam@apexpostech.com>
2024-02-15 12:55:17 -0800

    Implemented Prisma with Neon.tech, and set up a create user server action.

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,37 @@
 
 import * as z from 'zod';
 import { RegisterSchema } from "../schemas/index"
+import { Prisma } from '@prisma/client';
+import bcrypt from "bcrypt"
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
     if (!validatedFields.success) {
         return {error: "Invalid Fields! "}
+    }  
+    console.log("validatedFields,", validatedFields)
+
+    const { email, password, name } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const exisitingUser = await getUserByEmail(email)
+
+    if(exisitingUser) {
+        return {error: "Email is already in use."}
     }
-    return {success: "Email Sent!"}
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    //TODO: SEND VERIFICATION TOKEN EMAIL
+
+    return {success: "User Created!"}
 }
